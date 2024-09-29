e18982c722b071f4e42a2bb8d8720820f811e584
dhara-maru <dharamaru406@gmail.com>
2024-09-29 13:31:13 +0530

    Implemented login

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,9 @@
+from pyexpat.errors import messages
 from django . shortcuts import render, redirect
 from django.contrib.auth.models import User
 from todo import models
 from todo.models import todoclass
+from django.contrib.auth import authenticate, login as auth_login
 
 def signup(request):
     if request.method=="POST":
@@ -17,4 +19,15 @@ def signup(request):
     return render(request, 'signup.html')
 
 def login(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        pwd = request.POST.get('pwd')
+        
+        user=authenticate(request, username=name, password=pwd) #'username' and 'password' thse params are built-in django authentication
+        if user is not None:
+            auth_login(request, user)
+            return redirect('/todo')
+        else:
+            messages.error(request, 'Invalid username or password.')
+            return redirect('/login')
     return render(request, 'login.html')
