219876d8a96c6491e053cc01a3bcd8a26d61eff2
dhara-maru <dharamaru406@gmail.com>
2024-10-01 18:48:56 +0530

    Completed the ToDo prj after changing a little UI

--- todo/views.py
+++ todo/views.py
@@ -46,20 +46,6 @@ def login(request):
             
     return render(request, 'login.html')
 
-# def login(request):
-#     if request.method == "POST":
-#         name = request.POST.get('name')
-#         pwd = request.POST.get('pwd')
-        
-#         user=authenticate(request, username=name, password=pwd) #'username' and 'password' thse params are built-in django authentication
-#         if user is not None:
-#             auth_login(request, user)
-#             return redirect('/todo')
-#         else:
-#             messages.error(request, 'Invalid username or password.')
-#             return redirect('/login')
-#     return render(request, 'login.html')
-
 
 @login_required(login_url='/login') #inbuilt login decorator from django
 def todo(request):
